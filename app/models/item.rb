5c2bb7f7acc1d21931c6dcf43aa34352eb148b81
ochi0218 <ochi0218@gmail.com>
2014-03-30 14:42:22 +0900

    商品モデルの入力検証を追加。

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
+#
+# 商品
+#
 class Item < ActiveRecord::Base
+  mount_uploader :item_image, ItemImageUploader
+
+  validates :name, :price, :sort_no, presence: true
+  validates :item_image, file_size: { maximum: 0.5.megabytes.to_i }
+  validates_numericality_of :price, :sort_no , greater_than: 0
 end
