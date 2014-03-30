5a25f2dcaf7e2988d5a47e3e333f51c245fc6607
ochi0218 <ochi0218@gmail.com>
2014-03-30 15:07:05 +0900

    商品画像の属性名が間違っていたので修正。

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,9 +2,9 @@
 # 商品
 #
 class Item < ActiveRecord::Base
-  mount_uploader :item_image, ItemImageUploader
+  mount_uploader :image, ItemImageUploader
 
   validates :name, :price, :sort_no, presence: true
-  validates :item_image, file_size: { maximum: 0.5.megabytes.to_i }
+  validates :image, file_size: { maximum: 0.5.megabytes.to_i }
   validates_numericality_of :price, :sort_no , greater_than: 0
 end
