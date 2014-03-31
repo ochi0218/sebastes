c72b0a44c8f1c00cc4d061c6c2b6a3d0df2c5055
ochi0218 <ochi0218@gmail.com>
2014-04-01 00:05:32 +0900

    クーポンコードのヒントを追加

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -5,6 +5,6 @@ class Coupon < ActiveRecord::Base
   belongs_to :user
   validates :code, :point, presence: true
   validates :code, uniqueness: true
-  validates :code, format: { with: /[0-9a-zA-Z]{4,}-[0-9a-zA-Z]{4,}-[0-9a-zA-Z]{4,}-[0-9a-zA-Z]{4,}/ }
+  validates :code, format: { with: /[0-9a-zA-Z]{12,}/ }
   validates_numericality_of :point, greater_than: 0
 end
