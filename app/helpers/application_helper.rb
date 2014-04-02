9e07784dbb02b97cfbc7ebfb0979588224c41457
ochi0218 <ochi0218@gmail.com>
2014-04-02 18:10:07 +0900

    出力フォーマットを修正。

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def boolean_to_human(value)
+    value ? t('boolean.human.yes_or_true') : t('boolean.human.no_or_false')
+  end
 end
--- app/helpers/coupons_helper.rb
+++ app/helpers/coupons_helper.rb
@@ -1,2 +1,6 @@
 module CouponsHelper
+  def coupon_code_to_human(value)
+    return value if value.length != 12
+    "#{value[0..3]}-#{value[4..7]}-#{value[8..11]}"
+  end
 end
