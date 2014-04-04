5c3a21fecd1b6598d47d78e11e2b4f4ca2a6edcd
ochi0218 <ochi0218@gmail.com>
2014-04-04 11:36:05 +0900

    ユーザのプロフィール画像アップローダを追加。

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :coupon_logs
 
+  mount_uploader :profile_image, UserProfileImageUploader
+
   validates :destination_zip_code, format: { with: /\d{3,}-\d{4,}/ }, allow_blank: true
+  validates :profile_image, file_size: { maximum: 0.5.megabytes.to_i }
 end
