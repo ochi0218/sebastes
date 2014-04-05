7a4be8932f97f013d4df58fef9a1cd8f1e9b9ded
ochi0218 <ochi0218@gmail.com>
2014-04-05 16:13:53 +0900

    更新時にパスワードが送信されなかった場合は無視するように修正。

--- app/models/provide_user.rb
+++ app/models/provide_user.rb
@@ -1,5 +1,19 @@
+#
+# 業務アカウント
+#
 class ProvideUser < ActiveRecord::Base
   devise :database_authenticatable, :rememberable, :trackable, :validatable
 
+  before_validation :blank_password_to_nil
   validates :provider_name, presence: true
+
+  private
+
+  #
+  # 空白のパスワードをnilにする
+  #
+  def blank_password_to_nil
+    self.password = nil if self.password.blank?
+    self.password_confirmation = nil if self.password_confirmation.blank?
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,18 @@ class User < ActiveRecord::Base
 
   mount_uploader :profile_image, UserProfileImageUploader
 
+  before_validation :blank_password_to_nil
   validates :destination_zip_code, format: { with: /\d{3,}-\d{4,}/ }, allow_blank: true
   validates :profile_image, file_size: { maximum: 0.5.megabytes.to_i }
+
+  private
+
+  #
+  # 空白のパスワードをnilにする
+  #
+  def blank_password_to_nil
+    self.password = nil if self.password.blank?
+    self.password_confirmation = nil if self.password_confirmation.blank?
+  end
 end
+
