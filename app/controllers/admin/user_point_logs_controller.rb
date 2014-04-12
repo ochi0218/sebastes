f81e96ee7ee3ecb86d08e822d4fd1f6d64bf4934
ochi0218 <ochi0218@gmail.com>
2014-04-12 21:35:41 +0900

    日記一覧のデザインを修正。

--- app/controllers/admin/user_point_logs_controller.rb
+++ app/controllers/admin/user_point_logs_controller.rb
@@ -26,7 +26,7 @@ class Admin::UserPointLogsController < Admin::ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find(params[:user_id])
+      @user = User.find(params[:user_id], lock: true)
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
