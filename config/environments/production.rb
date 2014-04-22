7a5e817a48675418ea1fc090722e8b30109fcc8b
ochi0218 <ochi0218@gmail.com>
2014-04-22 20:51:01 +0900

    メールの設定を修正。

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Sebastes::Application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: 'sebastes.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'sebastes.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => 'smtp.gmail.com',
