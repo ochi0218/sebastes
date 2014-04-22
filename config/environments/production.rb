a40693987b76e5119332e4dc35db335513635b53
ochi0218 <ochi0218@gmail.com>
2014-04-22 20:36:50 +0900

    メール設定を追加。

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,18 @@ Sebastes::Application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: 'sebastes.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :authentication => :plain,
+    :user_name => ENV['EMAIL_SMTP_USER_NAME'],
+    :password => ENV['EMAIL_SMTP_PASSWORD'],
+    :enable_starttls_auto => true
+  }
+                                      
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
   config.i18n.fallbacks = true
